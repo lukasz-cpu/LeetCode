d836772a95ce174b8f736d63e4a2c25cc538b4b2
lukasz-cpu <lukasz.popek@hotmail.com>
2024-12-03 17:54:38 +0100

    palidrome

--- LeetCode/src/valid/palidrome/ValidPalidrome.java
+++ LeetCode/src/valid/palidrome/ValidPalidrome.java
@@ -1,7 +1,32 @@
 package valid.palidrome;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ValidPalidrome {
     public static void main(String[] args) {
-        
+        String s = "A man, a plan,  canal: Panama";
+        System.out.println(isPalindrome(s));
+    }
+
+    public static boolean isPalindrome(String s) {
+        String normalized = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        char[] normalizedCharArray = normalized.toCharArray();
+
+        List<String> reversedSequence = new ArrayList<>();
+
+        for (int i = normalizedCharArray.length - 1; i >= 0; i--) {
+            reversedSequence.add(String.valueOf(normalizedCharArray[i]));
+        }
+
+        char[] reversedCharArray = new char[normalizedCharArray.length];
+
+
+        for (int y = 0; y < reversedCharArray.length; y++) {
+            reversedCharArray[y] = reversedSequence.get(y).toCharArray()[0];
+        }
+
+        return Arrays.equals(reversedCharArray, normalizedCharArray);
     }
 }
