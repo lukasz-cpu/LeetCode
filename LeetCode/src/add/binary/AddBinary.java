7eb7606934fd7b6250845eadce1b42c8725f6021
lukasz-cpu <lukasz.popek@hotmail.com>
2025-01-03 16:44:23 +0100

    Create AddBinary.java

---
+++ LeetCode/src/add/binary/AddBinary.java
@@ -0,0 +1,41 @@
+package add.binary;
+
+public class AddBinary {
+    public static void main(String[] args) {
+        String a = "1010";
+        String b = "1011";
+
+
+        System.out.println(addBinary(a, b));
+
+    }
+
+    public static String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int aLength = a.length() - 1;
+        int bLength = b.length() - 1;
+        int temp = 0;
+
+        while (aLength >= 0 || bLength >= 0) {
+            int sum = temp;
+            if (aLength >= 0) {
+                sum += a.charAt(aLength) - '0';
+                aLength--;
+            }
+            if (bLength >= 0) {
+                sum += b.charAt(bLength) - '0';
+                bLength--;
+            }
+
+            sb.insert(0, sum % 2);
+            temp = sum / 2;
+        }
+
+        if (temp != 0) {
+            sb.insert(0, 1);
+        }
+
+        return sb.toString();
+    }
+
+}
