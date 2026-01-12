764e2dbf837d9619aae4fcbfe0d162d3a6e6282b
lukasz-cpu <lukasz.popek@hotmail.com>
2026-01-12 12:47:21 +0100

    Update RescueBoats.java

--- LeetCode/src/rescue/boats/RescueBoats.java
+++ LeetCode/src/rescue/boats/RescueBoats.java
@@ -1,11 +1,13 @@
 package rescue.boats;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 public class RescueBoats {
     public static void main(String[] args) {
-        int[] people = {3, 2, 2, 1};
+        int[] people = {3, 2, 2, 1, 2, 3, 3, 1, 2};
         int limit = 3;
 
         System.out.println(numRescueBoats(people, limit));
@@ -13,5 +15,31 @@ public class RescueBoats {
 
     public static int numRescueBoats(int[] people, int limit) {
         Arrays.sort(people);
+        int left = 0;
+        int right = people.length - 1;
+        int boats = 0;
+
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                left++;
+            }
+            right--;
+            boats++;
+        }
+
+        return boats;
     }
+
 }
+
+
+
+//        3
+//        3
+//        3
+//        2
+//        2
+//        2
+//        2
+//        1
+//        1
